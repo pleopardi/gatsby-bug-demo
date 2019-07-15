0959634da5cdc3bd3f0307b1b6e0484d5c06d37c
Paolo Leopardi <leopardipaolo@gmail.com>
2019-07-15 21:32:26 +0200

    refactor(BaseLayout.js):
    update BaseLayout global styling

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -10,7 +10,7 @@ const styles = {
     body: {
       boxSizing: "border-box",
     },
-    "h1, h2, h3, h4, h5, h6, img, p": {
+    "h1, h2, h3, h4, h5, h6, hr, img, p": {
       margin: 0,
       padding: 0,
     },
