bb2cce482610151a76d55b96a3dd6bc4a0e3186c
Paolo Leopardi <leopardipaolo@gmail.com>
2019-07-15 21:31:59 +0200

    refactor(Seo.js):
    update Seo component

---
+++ src/components/common/Seo.js
@@ -0,0 +1,83 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Helmet from "react-helmet";
+import { graphql, useStaticQuery } from "gatsby";
+
+function Seo({ description, lang, meta, title }) {
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            author
+            description
+            title
+          }
+        }
+      }
+    `
+  );
+
+  const metaDescription = description || site.siteMetadata.description;
+
+  const metaArray = [
+    {
+      name: "description",
+      content: metaDescription,
+    },
+    {
+      property: "og:title",
+      content: title,
+    },
+    {
+      property: "og:description",
+      content: metaDescription,
+    },
+    {
+      property: "og:type",
+      content: "website",
+    },
+    {
+      name: "twitter:card",
+      content: "summary",
+    },
+    {
+      name: "twitter:creator",
+      content: site.siteMetadata.author,
+    },
+    {
+      name: "twitter:title",
+      content: title,
+    },
+    {
+      name: "twitter:description",
+      content: metaDescription,
+    },
+  ].concat(meta);
+
+  return (
+    <Helmet
+      htmlAttributes={{
+        lang,
+      }}
+      meta={metaArray}
+      title={title}
+      titleTemplate={"%s | ${site.siteMetadata.title}"}
+    />
+  );
+}
+
+Seo.defaultProps = {
+  description: "",
+  lang: "en",
+  meta: [],
+};
+
+Seo.propTypes = {
+  description: PropTypes.string,
+  lang: PropTypes.string,
+  meta: PropTypes.arrayOf(PropTypes.object),
+  title: PropTypes.string.isRequired,
+};
+
+export default Seo;
