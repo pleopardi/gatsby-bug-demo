0088b4aa4c6b6924c23e101c58fa237a0f7a5ae9
Paolo Leopardi <leopardipaolo@gmail.com>
2019-07-15 21:36:05 +0200

    refactor(HomeLayout.js):
    update styling

--- src/components/HomeLayout.js
+++ src/components/HomeLayout.js
@@ -9,14 +9,23 @@ const styles = {
     marginRight: "auto",
     padding: "1rem",
 
-    "h3,p": {
+    h1: {
+      marginBottom: "1.2rem",
       marginTop: "1.2rem",
+    },
+    "h2, h3, h4, h5, h6": {
+      marginBottom: "0.8rem",
+      marginTop: "0.8rem",
+    },
+    p: {
+      marginBottom: "0.6rem",
+      marginTop: "0.6rem",
     },
   },
 };
 
 function HomeLayout({ children }) {
-  return <section css={styles.wrapper}>{children}</section>;
+  return <main css={styles.wrapper}>{children}</main>;
 }
 
 HomeLayout.propTypes = {
