964e2d3f0a630cff23ce026f32b72f3a50b891e9
Paolo Leopardi <leopardipaolo@gmail.com>
2019-07-30 22:57:26 +0200

    refactor(index.js):
    update styling and add logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,32 +3,52 @@ import { graphql } from "gatsby";
 import CssBaseline from "../components/CssBaseline";
 import Footer from "../components/common/Footer.atom";
 import HomeLayout from "../components/HomeLayout";
+import Logo from "../../content/assets/logo.svg";
 import Post from "../components/home/Post.organism";
 import PostList from "../components/home/PostList.organism";
 import Seo from "../components/common/Seo";
 import Spacer from "../components/common/Spacer.atom";
 import StyledLink from "../components/common/StyledLink.atom";
 
+const styles = {
+  blogTitleLink: {
+    textDecoration: "none",
+  },
+  header: {
+    display: "flex",
+    alignItems: "center",
+    marginBottom: "2.4rem",
+  },
+  logo: {
+    height: "4rem",
+    width: "4rem",
+  },
+};
+
 function BlogIndex({ data }) {
-  const siteTitle = data.site.siteMetadata.title;
   const firstPost = data.firstPost.nodes[0];
   const otherPosts = data.otherPosts.nodes;
+  const siteTitle = data.site.siteMetadata.title;
 
   return (
     <Fragment>
-      <Seo lang="it" title="Articoli del blog" />
+      <Seo lang="it" title="Home" />
       <CssBaseline />
       <HomeLayout>
-        <header>
-          <StyledLink to="/">
+        <header css={styles.header}>
+          <Logo css={styles.logo} />
+          <Spacer width="1rem" />
+          <StyledLink style={styles.blogTitleLink} to="/">
             <h1>{siteTitle}</h1>
           </StyledLink>
         </header>
-        <Post post={firstPost} />
-        <Spacer height="4rem" />
-        <hr />
-        <Spacer height="4.6rem" />
-        <PostList posts={otherPosts} />
+        <main>
+          <Post post={firstPost} style={{ marginBottom: 0 }} />
+          <Spacer height="4rem" />
+          <hr />
+          <Spacer height="4rem" />
+          <PostList posts={otherPosts} />
+        </main>
         <Spacer height="4rem" />
         <Footer />
       </HomeLayout>
@@ -91,11 +111,3 @@ export const pageQuery = graphql`
     }
   }
 `;
-
-/*
-
-/ <Layout location={this.props.location} title={siteTitle}>
-      // <SEO title="All posts" />
-      // <Bio />
-
-*/
