444cc24a5ecd0add0a51ddabdd69e2023bbe531b
Paolo Leopardi <leopardipaolo@gmail.com>
2019-07-31 19:38:15 +0200

    refactor(PostLayout.js):
    update styling

--- src/components/PostLayout.js
+++ src/components/PostLayout.js
@@ -11,14 +11,30 @@ const styles = {
     display: "flex",
     flexDirection: "column",
 
-    "h1, h2, h3, h4, h5, h6, img": {
+    a: {
+      color: "#71151a",
+      fontWeight: 500,
+    },
+    blockquote: {
+      fontStyle: "italic",
+    },
+    h1: {
+      fontWeight: 600,
+    },
+    "h1, h2": {
+      marginBottom: "1rem",
+      marginTop: "2rem",
+    },
+    "h3, h4, h5, h6": {
       marginBottom: "0.8rem",
-      marginTop: "0.8rem",
+      marginTop: "1.8rem",
     },
-    "h1, h2, h3, h4, h5, h6, p": {
-      textAlign: "justify",
+    "img, p": {
+      marginBottom: "1.2rem",
+      marginTop: "1.2rem",
     },
-    "p, ul": {
+    li: {
+      lineHeight: "1.6rem",
       marginBottom: "0.6rem",
       marginTop: "0.6rem",
     },
