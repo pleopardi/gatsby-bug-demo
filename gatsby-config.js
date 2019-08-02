15c19e072cdd44f9ae95c65d02902bdb8a269ae2
Paolo Leopardi <leopardipaolo@gmail.com>
2019-08-02 17:07:45 +0200

    chore(gatsby-config.js):
    add Google Analytics tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-google-analytics",
       options: {
-        //trackingId: "ADD YOUR TRACKING ID HERE",
+        trackingId: "UA-145064596-1",
       },
     },
     {
