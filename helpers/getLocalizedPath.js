f6760bd652d69949810c13720c88aac4ef09bdf5
Paolo Leopardi <leopardipaolo@gmail.com>
2019-08-14 16:04:16 +0200

    docs(helpers/getLocalizedPath.js):
    add description and warns for usage

--- helpers/getLocalizedPath.js
+++ helpers/getLocalizedPath.js
@@ -1,5 +1,10 @@
 const { config } = require("../content/intl/config");
 
+/**
+ * @description Localizes a path by prefixing the locale.
+ * When the locale is the default one, it returns the input path unchanged.
+ * Do not use with already localized paths or slugs!
+ */
 module.exports = function getLocalizedPath({ locale, path }) {
   const isDefaultLocale = locale === config.defaultLocale;
 
