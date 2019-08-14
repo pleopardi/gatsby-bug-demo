d57e079e71aa15a3f826845b3be023d927b3c59d
Paolo Leopardi <leopardipaolo@gmail.com>
2019-08-14 11:02:08 +0200

    chore(intl/config.js):
    add localesConfig object

--- content/intl/config.js
+++ content/intl/config.js
@@ -3,6 +3,18 @@ const config = {
   locales: ["it", "en"],
 };
 
+const localesConfig = {
+  it: {
+    dateFormat: "DD/MM/YYYY",
+    name: "Italiano",
+  },
+  en: {
+    dateFormat: "DD/MM/YYYY",
+    name: "English",
+  },
+};
+
 module.exports = {
   config,
+  localesConfig,
 };
