8792f2a1c0a68491b9feed814c696d84fe409b6f
Paolo Leopardi <leopardipaolo@gmail.com>
2019-08-14 19:11:08 +0200

    feat(pages/404.js):
    use translations

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,24 +1,52 @@
 import React, { Fragment } from "react";
-import { Link } from "gatsby";
+import { graphql, Link } from "gatsby";
 import CookieBanner from "../components/CookieBanner";
 import CssBaseline from "../components/CssBaseline";
 import NotFoundLayout from "../layouts/NotFoundLayout";
 import { Seo } from "../components/common";
 
-class NotFoundPage extends React.Component {
-  render() {
-    return (
-      <Fragment>
-        <Seo lang="it" title="Pagina inesistente" />
-        <CssBaseline />
-        <CookieBanner />
-        <NotFoundLayout>
-          <h1>Pagina inesistente</h1>
-          <Link to="/">Vai alla home</Link>
-        </NotFoundLayout>
-      </Fragment>
-    );
-  }
+function NotFoundPage({ data, pageContext }) {
+  const locale = pageContext.locale;
+  const { link, title } = data.translations.nodes[0].messages[locale];
+
+  return (
+    <Fragment>
+      <Seo lang={locale} title={title} />
+      <CssBaseline />
+      <CookieBanner />
+      <NotFoundLayout>
+        <h1>{title}</h1>
+        <Link to="/">{link}</Link>
+      </NotFoundLayout>
+    </Fragment>
+  );
 }
 
+// TODO: add propTypes
+
 export default NotFoundPage;
+
+export const pageQuery = graphql`
+  query get404Data {
+    translations: allFile(
+      filter: {
+        extension: { eq: "json" }
+        name: { eq: "notFound" }
+        sourceInstanceName: { eq: "intl" }
+      }
+    ) {
+      nodes {
+        messages: childTranslationsJson {
+          en {
+            link
+            title
+          }
+          it {
+            link
+            title
+          }
+        }
+      }
+    }
+  }
+`;
